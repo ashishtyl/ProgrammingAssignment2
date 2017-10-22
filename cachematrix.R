a6441d56c76d66a5215a7c561f802f58b6362ef3
ashish <ashishtyl@gmail.com>
2017-10-23 02:38:51 +0800

    second iteration

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,9 @@
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
-cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
-}
-
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 # Set the matrix and its inverse to the environment variable, 
-
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
   
@@ -44,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
   
   setInverse <- function(inv) inverse <<- inv
   
-  getInverse <- function() inv
+  getInverse <- function() inverse
   
   list(set = set, 
        get = get,
@@ -60,24 +33,22 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   
-  #get cached matrix   
-  i <- x$get 
-  #check if cached matrix matches matrix supplied
-  if(i==x){
+  #check matrix inverse exits
+  if(!is.null(x$getInverse())){ 
     #return cached invese 
     message("Matrix in cache. Getting inverse...")
-    return(x$getInverse)
+    x$getInverse()
   }
+  
   #matrix not in cache 
   else {
     #calculate inverse 
-    t <- solve(x$get)
+    t <- solve(x$get())
     ti <- solve(t)
     
     #set in cache
     x$set(t)
     x$setInverse(ti)
-  
     ti
   }
 }
